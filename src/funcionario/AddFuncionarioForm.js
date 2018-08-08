0440ec290f1424d8d4c2a827c57e24447cc9ed19
Daniel Ramos <ramos.danielferreira@gmail.com>
2018-08-07 23:37:25 -0300

    Removido Redux para form e usando HoC do Formik

--- src/funcionario/AddFuncionarioForm.js
+++ src/funcionario/AddFuncionarioForm.js
@@ -1,103 +1,90 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import Grid from 'react-bootstrap/lib/Grid';
-import Panel from 'react-bootstrap/lib/Panel';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-import Button from 'react-bootstrap/lib/Button';
-import { withRouter } from 'react-router-dom';
 import { render } from 'react-dom';
 import { withFormik } from 'formik';
-import Yup from 'yup';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-class FuncionarioInnerForm extends React.Component {
+const MyForm = props => {
+    const {
+        values,
+        touched,
+        errors,
+        isSubmitting,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        handleReset,
+        dirty,
+    } = props;
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email" style={{ display: 'block' }}>
+                Email
+        </label>
+            <input
+                id="email"
+                placeholder="Enter your email"
+                type="text"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                    errors.email && touched.email ? (
+                        'text-input error'
+                    ) : (
+                            'text-input'
+                        )
+                }
+            />
+            {errors.email &&
+                touched.email && (
+                    <div className="input-feedback">{errors.email}</div>
+                )}
 
-    handleSubmit(event) {
-        event.preventDefault();
-    }
-    render() {
-        const {
-            values,
-            touched,
-            errors,
-            dirty,
-            isSubmitting,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            handleReset,
-        } = this.props;
+            <button
+                type="button"
+                className="outline"
+                onClick={handleReset}
+                disabled={!dirty || isSubmitting}
+            >
+                Reset
+        </button>
+            <button type="submit" disabled={isSubmitting}>
+                Submit
+        </button>
+        </form>
+    );
+};
 
-        return (
-            <Grid>
-                <form onSubmit={handleSubmit}>
-                    <Panel>
-                        <Panel.Heading>Cadastro de funcionários</Panel.Heading>
-                        <Panel.Body>
-                            <Row className="show-grid">
-                                <Col lg={4} xs={12}>
-                                    <label>Nome:</label>
-                                    <input type="text" className="form-control" />
-                                </Col>
-                                <Col lg={4} xs={12}>
-                                    <label>E-mail:</label>
-                                    <input
-                                        id="email"
-                                        placeholder="Seu email"
-                                        type="text"
-                                        value={values.email}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        className={errors.email && touched.email ? 'text-input error' : 'text-input'}
-                                    />
-                                </Col>
-                                <Col lg={4} xs={12}>
-                                    <label>Data de nascimento:</label>
-                                    <input type="text" className="form-control" />
-                                </Col>
-                            </Row>
-                        </Panel.Body>
-                        <Panel.Footer className="text-right">
-                            <button type="submit" disabled={isSubmitting}>
-                                Submit
-                            </button>
-                        </Panel.Footer>
-                    </Panel>
-                </form>
-            </Grid>
-        )
-    }
-}
 
-const FuncionarioForm = withFormik({
+const MyEnhancedForm = withFormik({
     mapPropsToValues: () => ({ email: '' }),
-    validationSchema: Yup.object().shape({
-        email: Yup.string()
-            .email('Invalid email address')
-            .required('Email is required!'),
-    }),
+
+    // Custom sync validation
+    validate: values => {
+        debugger;
+        let errors = {};
+        if (!values.email) {
+            errors.email = 'Required';
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+                values.email
+            )
+        ) {
+            errors.email = 'Invalid email address';
+        }
+        return errors;
+    },
+
     handleSubmit: (values, { setSubmitting }) => {
+        debugger;
         setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
         }, 1000);
     },
+
     displayName: 'BasicForm', // helps with React DevTools
-})(FuncionarioInnerForm)
+})(MyForm);
 
-const AddFuncionarioForm = function ({ match, onAddAuthor }) {
-    return <Grid>
-        <FuncionarioForm />
-    </Grid>;
-}
-
-function mapDispatchToProps(dispatch, props) {
-    return {
-        onAddAuthor: (author) => {
-            dispatch({ type: 'ADD_AUTHOR', author });
-            props.history.push('/');
-        }
-    };
-}
-
-export default withRouter(connect(() => { }, mapDispatchToProps)(AddFuncionarioForm));
+export default withRouter(MyEnhancedForm);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
 import * as Redux from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import * as ReactRedux from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import AddFuncionarioForm from './funcionario/AddFuncionarioForm';
-import { render } from 'react-dom';
-import { withFormik } from 'formik';
-import Yup from 'yup';
 
 function reducer(
     state = {},
@@ -17,90 +14,13 @@ function reducer(
     }
 }
 
-
 let store = Redux.createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-
-const MyInnerForm = props => {
-    const {
-        values,
-        touched,
-        errors,
-        dirty,
-        isSubmitting,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        handleReset,
-    } = props;
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email" style={{ display: 'block' }}>
-                Email
-        </label>
-            <input
-                id="email"
-                placeholder="Enter your email"
-                type="text"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className={errors.email && touched.email ? 'text-input error' : 'text-input'}
-            />
-            {errors.email &&
-                touched.email && <div className="input-feedback">{errors.email}</div>}
-
-            <button
-                type="button"
-                className="outline"
-                onClick={handleReset}
-                disabled={!dirty || isSubmitting}
-            >
-                Reset
-        </button>
-            <button type="submit" disabled={isSubmitting}>
-                Submit
-        </button>
-        </form>
-    );
-};
-
-const EnhancedForm = withFormik({
-    mapPropsToValues: () => ({ email: '' }),
-    validationSchema: Yup.object().shape({
-        email: Yup.string()
-            .email('Invalid email address')
-            .required('Email is required!'),
-    }),
-    handleSubmit: (values, { setSubmitting }) => {
-        debugger;
-        setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-        }, 1000);
-    },
-    displayName: 'BasicForm', // helps with React DevTools
-})(MyInnerForm);
-
-const App = () => (
-    <div className="app">
-        <h1>
-            Basic{' '}
-            <a href="https://github.com/jaredpalmer/formik" target="_blank" rel="noopener">
-                Formik
-        </a>{' '}
-            Demo
-      </h1>
-
-        <EnhancedForm />
-    </div>
-);
-
-ReactDOM.render(<BrowserRouter>
-    <ReactRedux.Provider store={store}>
+ReactDOM.render(<BrowserRouter store={store}>
+    <ReactRedux.Provider>
         <React.Fragment>
             <Route exact path="/" component={AddFuncionarioForm} />
         </React.Fragment>
